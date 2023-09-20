3e34e843779f9b94277e2f8caccfbcebd38838e7
Milena-Romero <milenajulio25@gmail.com>
2023-09-19 22:04:52 -0500

    configuración proyecto -1er template Inicio

--- principal/urls.py
+++ principal/urls.py
@@ -1,5 +1,3 @@
-"""
-URL configuration for principal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
