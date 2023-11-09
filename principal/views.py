85fdfb3c2b7a67317c9397e065fece35f69cda45
Milena-Romero <milenajulio25@gmail.com>
2023-11-09 13:05:14 -0500

    Ultimo..
    Ultimo..

---
+++ principal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def inicio(request):
+    titulo = "Inicio"
+    context={
+        "titulo": titulo,
+    }
+    return render(request, 'index.html', context)
+
+    
